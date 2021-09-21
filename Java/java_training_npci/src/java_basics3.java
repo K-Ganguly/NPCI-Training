e793366f3af283ad114999256eb80e7e3fbffe95
kganguly <kganguly.main@gmail.com>
2021-09-21 14:05:06 +0530

    java basics - assignment 3 optimised

--- Java/java_training_npci/src/java_basics3.java
+++ Java/java_training_npci/src/java_basics3.java
@@ -8,32 +8,44 @@ public class java_basics3 {
         sc.nextLine();
 
         for (int i = 0, l = num.length(); i < l; i++) {
-            if (num.charAt(i) == '.')
+            if (num.charAt(i) == '.'){
                 isPoint = true;
+                break;
+            }
         }
 
         if (isPoint) {
             double numVal = Double.parseDouble(num);
+            Double makeNull = null;
             if (numVal >= Float.MIN_VALUE && numVal <= Float.MAX_VALUE){
-                System.out.println("Type : float");
+                float val = (float)numVal;
+                numVal = makeNull;
+                System.out.printf("Type of %f is : <float>", val);
             }
             else {
-                System.out.println("Type : double");
+                System.out.printf("Type of %f is : <double>", numVal);
             }
         }
         else {
             long numVal = Long.parseLong(num);
+            Long makeNull = null;
             if (numVal >= Byte.MIN_VALUE && numVal <= Byte.MAX_VALUE){
-                System.out.println("Type : byte");
+                byte val = (byte)numVal;
+                numVal = makeNull;
+                System.out.printf("Type of %d is : <byte>", val);
             }
             else if (numVal >= Short.MIN_VALUE && numVal <= Short.MAX_VALUE){
-                System.out.println("Type : short");
+                short val = (byte)numVal;
+                numVal = makeNull;
+                System.out.printf("Type of %d is : <short>", val);
             }
             else if (numVal >= Integer.MIN_VALUE && numVal <= Integer.MAX_VALUE){
-                System.out.println("Type : int");
+                int val = (byte)numVal;
+                numVal = makeNull;
+                System.out.printf("Type of %d is : <int>", val);
             }
             else {
-                System.out.println("Type : long");
+                System.out.printf("Type of %f is : <long>", numVal);
             }
         }
         sc.close();
