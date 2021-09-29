b5ab9ebdd79d4693e7a15b6b73417ba54a3284f7
kganguly <kganguly.main@gmail.com>
2021-09-29 10:54:26 +0530

    lambda assignment 1 added

--- Python/python_basics/basics_lambdas.py
+++ Python/python_basics/basics_lambdas.py
@@ -1,5 +1,24 @@
+elements_list = list()
+elem_dict = dict()
 
-def sort_dict(nums_dict) :
+# Adding element to the list : 
+def add_element() :
+    elem = int(input("Enter the element you want to add : "))
+    elements_list.append(elem)
+    return "After adding {}, List is {} ".format(elem, elements_list)
+
+# Creating a dictionary from the list : 
+def create_dict() :
+    if len(elements_list) < 2 :
+        return "List is too short to be made a dictionary"
+    else : 
+        for num in elements_list : 
+            elem_dict[num] = elem_dict.get(num, 0) + 1
+        return "Created Dictionary from List : {} is \n{}".format(elements_list, elem_dict)
+
+
+# Sort the dict by value 
+def sort_dict() :
     # helper functions 
     def merge(left_keys, left_vals, right_keys, right_vals) :
         i, j = 0, 0
@@ -43,20 +62,23 @@ def sort_dict(nums_dict) :
             num_dict_vals.append(val)
         return num_dict_keys, num_dict_vals
 
-    num_keys, num_vals = dict_to_list(nums_dict)
+    num_keys, num_vals = dict_to_list(elem_dict)
     sorted_keys, sorted_vals = merge_sort(num_keys, num_vals)
     sorted_dict = {key : val for key, val in zip(sorted_keys, sorted_vals)}
-    return sorted_dict
-print("Enter the number of the elements of the dictionary : ")
-n = int(input())
-print("Enter the items of the dictionary in key-value order : ")
-num_dict = dict()
-for _ in range(n) :
-    key, value = input().split()
-    num_dict[key] = value 
+    return "Dictionary Sorted by Value : {}".format(sorted_dict)
 
-print("Before Sorting : ")
-print(num_dict)
-sorted_dict = sort_dict(num_dict)
-print("After Sorting : ")
-print(sorted_dict)
+def exit_menu() : 
+    return "Exiting ........... "
+
+choice = 1
+while choice != 4 : 
+    choice = int(input("\n\n1. Add a new element to the list\n2. Create a dictionary from the list\n3. Sort the dictionary in descending order based on value\n4.Exit\n"))
+    if choice == 1 :
+        print(add_element())
+    elif choice == 2 : 
+        print(create_dict())
+    elif choice == 3 :
+        print(sort_dict())
+    else : 
+        print(exit_menu())
+    
